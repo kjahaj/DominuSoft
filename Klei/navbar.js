2b16c5f7a11235aa2267f10b8615e9fc15dd22da
kleij <kleijahaj08@gmail.com>
2023-07-17 13:32:46 +0200

    Klei

---
+++ Klei/navbar.js
@@ -0,0 +1,12 @@
+let mainDiv = document.getElementsByClassName("menu")[0];
+let sandwich = document.getElementsByClassName("sandwich")[0];
+        
+function sandwichIcon() {
+    if (mainDiv.style.display == "none") {
+        mainDiv.style.display = "block";
+    } else {
+        mainDiv.style.display = "none";
+    }
+}
+
+sandwich.addEventListener("click", sandwichIcon);
