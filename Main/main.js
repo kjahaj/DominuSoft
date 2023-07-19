835535cdddd3b0cbce955c7a35d3d48ab2250ddb
kleij <kleijahaj08@gmail.com>
2023-07-19 10:49:54 +0200

    klei

--- Main/main.js
+++ Main/main.js
@@ -1,12 +1,12 @@
 let mainDiv = document.getElementsByClassName("menu")[0];
 let sandwich = document.getElementsByClassName("sandwich")[0];
-
+        
 function sandwichIcon() {
-    if (mainDiv.style.display === "none") {
+    if (mainDiv.style.display == "none") {
         mainDiv.style.display = "block";
     } else {
         mainDiv.style.display = "none";
     }
 }
-console.log("TEST");
+
 sandwich.addEventListener("click", sandwichIcon);
