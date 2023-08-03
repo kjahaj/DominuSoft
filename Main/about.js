f12a0f2981830f735cc23f7fa3cd167c994690b4
Ervis1003 <ervisbakalli@icloud.com>
2023-08-03 19:40:56 +0200

    1

---
+++ Main/about.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  if (menu.style.display === 'none') {
+    menu.style.display = 'flex';
+  } else {
+    menu.style.display = 'none';
+  }
+}
+
